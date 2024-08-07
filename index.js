e2dc324a1b2a941de940b48d5621df59937d9e5b
julio cesar <jcmcf2012@gmail.com>
2024-08-07 11:38:31 -0300

    feat: add class

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1 - Declare uma variável chamada nome e atribua a ela seu nome completo como uma string.
+
+// 2 - Declare uma variável idade como um número inteiro representando sua idade.
+
+// 3 - Concatene as variáveis nome e idade em uma string com a mensagem: "Meu nome é [nome] e eu tenho [idade] anos."
+
+// 4 - Declare uma variável maiorDeIdade e atribua a ela true se idade for 18 ou mais, caso contrário, false.
+
+// 5 - Declare uma variável altura em metros (como um número decimal).
