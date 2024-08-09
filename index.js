9f5ad8bc2e2f7fed05e57017fa60613336d770db
julio cesar <jcmcf2012@gmail.com>
2024-08-09 11:19:33 -0300

    feat: finish math class

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
 // Exercício 1: Soma de Dois Números
 // Declare duas variáveis numéricas, some-as e imprima o resultado no console.
+const a = 20;
+const b = 30;
+console.log("Exercicio 1: Soma ", a + b);
 
 // Exercício 2: Produto de Dois Números
 // Declare duas variáveis numéricas, multiplique-as e imprima o resultado no console.
+console.log("Exercicio 2: Multi ", a * b);
 
 // Exercício 3: Divisão e Resto
 // Divida um número por outro e imprima o resto.
+const c = 10;
+const d = 4;
+const e = 9;
+console.log("Exercicio 3: Resto ", c % d);
+// console.log("Par ou impar ", c % 2 === 0);
+// console.log("Par ou impar ", e % 2 === 0);
 
 // Exercício 4: Incremento e Decremento
 // Incremente e depois decremente um número, imprimindo o valor a cada passo.
+let f = 0;
+f++;
+console.log("Exercicio 4: Incremento ", f);
+f--;
+console.log("Exercicio 4: Decremento ", f);
 
 // Exercício 5: Potenciação
 // Calcule o resultado de um número elevado a outro número.
+let h = 10;
+console.log("Exercicio 5: Potenciacao ", h ** 2);
 
 // Exercício 6: Operador de Atribuição Composto
 // Use um operador de atribuição composto para somar e atribuir em uma mesma operação.
+let j = 10;
+j += 10;
+j *= 10;
+j /= 10;
+console.log("Exercicio 6: Atribuição Composto ", j);
 
 // Exercício 7: Conversão de String para Número
 // Converta uma string numérica para um número e faça uma operação aritmética com ela.
+// let k = Number("10");
+let k = +"10";
+let l = 5;
+console.log("Exercício 7: Conversão de String para Número ", k + l);
 
 // Exercício 8: Calcular a Área de um Retângulo
 // Calcule a área de um retângulo dado seu comprimento e largura.
+const comp = 4;
+const largura = 5;
+const area = comp * largura;
+console.log("Exercício 8: Calcular a Área de um Retângulo ", area);
 
 // Exercício 9: Reduzir um Número pela Metade
 // Divida um número pela metade e imprima o resultado.
+let i = 10;
+console.log("Exercício 9: Reduzir um Número pela Metade ", i / 2);
 
 // Exercício 10: Calculando a Média
 // Calcule a média de três números e imprima o resultado.
+const aluno1 = 10;
+const aluno2 = 7;
+const aluno3 = 8;
+const media = (aluno1 + aluno2 + aluno3) / 3;
+console.log("Exercício 10: Calculando a Média", i / 2);
+
+// console.log(media.toFixed(2));
+// console.log(Math.floor(media));
+// console.log(Math.ceil(media));
+// console.log(Math.round(media));
