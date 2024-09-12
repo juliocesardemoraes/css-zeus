25beb99d72580dc32b4afddef2c7efb2f1f094ab
juliocesardemoraes <jcmcf2012@gmail.com>
2024-09-12 08:36:58 -0300

    fix:format classes

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 // 1. Função de Saudação
-// Crie uma função chamada saudacao OK que receba um nome como parâmetro OK e imprima "Olá, [nome]!" no console.
+// Crie uma função chamada saudacao 
+// que receba um nome como parâmetro 
+// e imprima "Olá, [nome]!" no console.
+
 
 // 2. Função de Subtração
-// Crie uma função chamada subtracao que receba dois números como parâmetros e retorne a diferença entre eles.
+// Crie uma função chamada subtracao 
+// que receba dois números como parâmetros 
+// e retorne a diferença entre eles.
+
 
 // 3. Função de Multiplicação
-// Crie uma função chamada multiplicacao que receba dois números e retorne o produto deles.
+// Crie uma função chamada multiplicacao 
+// que receba dois números 
+// e retorne o produto deles.
+
 
 // 4. Função de Concatenar Strings
-// Crie uma função chamada concatenar que receba duas strings e retorne a concatenação delas.
+// Crie uma função chamada concatenar 
+// que receba duas strings 
+// e retorne a concatenação delas.
 
 // 5. Função para Encontrar o Maior Número
-// Crie uma função chamada maiorNumero que receba dois números e retorne o maior deles.
+// Crie uma função chamada maiorNumero 
+// que receba dois números 
+// e retorne o maior deles.
+
 
 // 6. Função de Verificação de Paridade
-// Crie uma função chamada parouimpar que receba um número e retorne true se for par, false caso contrário.
+// Crie uma função chamada parouimpar 
+// que receba um número e retorne true se for par, false caso contrário.
 
 // 7. Função de Conversão para Celsius
-// Crie uma função chamada paraCelsius que converta uma temperatura de Fahrenheit para Celsius.
+// Crie uma função chamada paraCelsius 
+// que converta uma temperatura de Fahrenheit para Celsius.
 // "Subtraímos a temperatura em ºF por 32 e dividimos o resultado por 1,8. "
 
+
 // 8. Função de Contagem de Caracteres
-// Crie uma função chamada contarCaracteres que receba uma string 
+// Crie uma função chamada contarCaracteres 
+// que receba uma string 
 // e retorne um numero com a contagem de todos os caracteres.
 
+
 // 9. Função de Média
-// Crie uma função chamada media que receba um array de números e retorne a média
+// Crie uma função chamada media 
+// que receba um array de números 
+// e retorne a média
+
--- indexAnswers.js
+++ indexAnswers.js
@@ -1,85 +1,104 @@
 // 1. Função de Saudação
-// Crie uma função chamada saudacao OK 
-// que receba um nome como parâmetro OK
+// Crie uma função chamada saudacao 
+// que receba um nome como parâmetro 
 // e imprima "Olá, [nome]!" no console.
 function saudacao(nome){
-    console.log(`Olá, ${nome}!`)
+    console.log(`Olá, ${nome}`)
 }
 saudacao("Júlio")
 
+
 // 2. Função de Subtração
-// Crie uma função chamada subtracao que receba dois números como parâmetros e retorne a diferença entre eles.
+// Crie uma função chamada subtracao 
+// que receba dois números como parâmetros 
+// e retorne a diferença entre eles.
 function subtracao(num1,num2){
-    return num1 - num2;
+    return  Math.abs(num1 - num2);
 }
-const sub = subtracao(2,3)
-console.log("O retorno da subtração foi: ", sub)
+const diff = subtracao(200,3000);
+console.log("O resultado da subtração é: ", diff)
+
 
 // 3. Função de Multiplicação
-// Crie uma função chamada multiplicacao que receba dois números e retorne o produto deles.
+// Crie uma função chamada multiplicacao 
+// que receba dois números 
+// e retorne o produto deles.
 function multiplicacao(num1,num2){
     return num1 * num2;
 }
-const multi = multiplicacao(2,3)
-console.log("O retorno da multiplicação foi: ", multi)
+
+const multi = multiplicacao(4,3)
+console.log("O resultado da multiplicacao é: ", multi)
+
 
 // 4. Função de Concatenar Strings
-// Crie uma função chamada concatenar que receba duas strings e retorne a concatenação delas.
-function concatenar(texto1,texto2){
-    return texto1 + texto2;
+// Crie uma função chamada concatenar 
+// que receba duas strings 
+// e retorne a concatenação delas.
+function concatenar(str1,str2){
+    return str1 + str2;
 }
-const conc = concatenar("A minha idade é ", "28")
-console.log("O retorno da concatenação foi: ", conc)
 
+const conc = concatenar("Meu nome é ", "Júlio")
+console.log("O resultado da concatenar é: ", conc)
 
 // 5. Função para Encontrar o Maior Número
-// Crie uma função chamada maiorNumero que receba dois números e retorne o maior deles.
+// Crie uma função chamada maiorNumero 
+// que receba dois números 
+// e retorne o maior deles.
 function maiorNumero(num1,num2){
- // ternário ou if/else
- return num1 > num2 ? num1 : num2
+    return num1 > num2 ? num1 : num2
 }
 
-const maior = maiorNumero(1,42)
-console.log("O retorno do maiorNumero foi: ", maior)
+const maior = maiorNumero(100,20);
+console.log("O resultado do maiorNumero é: ", maior)
+
 
 // 6. Função de Verificação de Paridade
-// Crie uma função chamada parouimpar que receba um número e retorne true se for par, false caso contrário.
+// Crie uma função chamada parouimpar 
+// que receba um número e retorne true se for par, false caso contrário.
 function parouimpar(num1){
-    return num1 % 2 === 0 ? true : false
+    return num1 % 2 === 0 ? true : false;
 }
-const epar = parouimpar(2)
-console.log("O retorno do parouimpar foi: ", epar)
 
+const epar = parouimpar(3);
+console.log("O resultado do parouimpar é: ", epar)
 
 // 7. Função de Conversão para Celsius
-// Crie uma função chamada paraCelsius que converta uma temperatura de Fahrenheit para Celsius.
+// Crie uma função chamada paraCelsius 
+// que converta uma temperatura de Fahrenheit para Celsius.
 // "Subtraímos a temperatura em ºF por 32 e dividimos o resultado por 1,8. "
 function paraCelsius(tempF){
-    return (tempF - 32) / 1.8
+    return Number(((tempF - 32) / 1.8).toFixed(2));
 }
 
-const far = paraCelsius(90)
-console.log("O retorno do paraCelsius foi: ", far.toFixed(2))
-
+const far = paraCelsius(90);
+console.log("O resultado do paraCelsius é: ", far)
 
 // 8. Função de Contagem de Caracteres
-// Crie uma função chamada contarCaracteres que receba uma string e retorne um numero com a contagem de todos os caracteres.
-function contarCaracteres(texto1){
-    return texto1.length;
+// Crie uma função chamada contarCaracteres 
+// que receba uma string 
+// e retorne um numero com a contagem de todos os caracteres.
+function contarCaracteres(str1){
+    return str1.length;
 }
-const total = contarCaracteres("Meu nome é Júlio")
-console.log("O retorno do contarCaracteres foi: ", total)
+const contagemChar = contarCaracteres("Julio César")
+console.log("O resultado do contarCaracteres é: ", contagemChar)
+
 
 // 9. Função de Média
-// Crie uma função chamada media que receba um array de números e retorne a média
-function media(arrayDeNumeros){
-    let soma = 0;
-    for(let i = 0; i < arrayDeNumeros.length; i++){
-        soma += arrayDeNumeros[i];
-    }
-    return soma / arrayDeNumeros.length
-}
-const array = [2,3,4] // 2 + 3 + 4 = (9  / 3) = 3
-const mediaTotal = media(array);
-console.log("O retorno da media foi: ", mediaTotal)
+// Crie uma função chamada media 
+// que receba um array de números 
+// e retorne a média
 
+function media(arrayNum){
+    let soma = 0;
+    for(let i = 0; i<arrayNum.length; i++){
+        soma += arrayNum[i];
+    }
+    return soma / arrayNum.length;
+}
+        //    0 1 2
+const nums = [2,3,4] // 2 + 3 + 4 = 9 / 3 = 3
+const mediaTotal = media(nums);
+console.log("O resultado do media é: ", mediaTotal)
