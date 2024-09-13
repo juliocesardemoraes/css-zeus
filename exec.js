be07f1d20af806d38db9b7651ddd146fa32f1656
juliocesardemoraes <jcmcf2012@gmail.com>
2024-09-13 08:27:53 -0300

    feat: add exercs

--- exec.js
+++ exec.js
@@ -1,6 +1,7 @@
 // Exercícios de 'CRUD'
-// GET - READ
-// 1. Crie um objeto chamado 'carro' com as propriedades 'marca', 'modelo', e 'ano'. Acesse e imprima cada uma das propriedades no console.
+// GET - CREATE/ READ
+// 1. Crie um objeto chamado 'carro' com as propriedades 'marca', 'modelo', e 'ano'.
+// Acesse e imprima cada uma das propriedades no console.
 
 // POST - INSERT
 // 2. Adicione uma nova propriedade 'cor' ao objeto 'carro' e atribua a ela o valor "vermelho".
@@ -9,13 +10,18 @@
 // 3. Modifique o valor da propriedade 'ano' do objeto 'carro' para o ano atual.
 
 // DELETE - DELETE
-// 4. Delete a propriedade 'modelo' do objeto 'carro' e tente acessar essa propriedade após a exclusão.
+// 4. Delete a propriedade 'modelo' do objeto 'carro'
+// e tente acessar essa propriedade após a exclusão.
 
 // Múltiplos Métodos
-// 5. Crie um objeto chamado 'pessoa' com as propriedades 'nome', 'idade', e 'cidade'. Modifique a 'idade' para ser 5 anos maior.
+// 5. Crie um objeto chamado 'pessoa' com as propriedades 'nome', 'idade', e 'cidade'.
+// Modifique a 'idade' para ser 5 anos maior.
 
-// 6. Acesse e imprima o valor da propriedade 'nome' do objeto 'pessoa' usando a notação de colchetes.
+// 6. Acesse e imprima o valor da propriedade 'nome' do objeto 'pessoa'
+// usando a notação de colchetes.
 
-// 7. Crie um loop 'for...in' para iterar sobre todas as propriedades do objeto 'pessoa' e imprima chave e valor.
+// 7. Crie um loop 'for...in' para iterar sobre todas as propriedades
+// do objeto 'pessoa' e imprima chave e valor.
 
-// 8. Adicione uma nova propriedade 'profissao' ao objeto 'pessoa' e atribua o valor "Engenheira".
+// 8. Adicione uma nova propriedade 'profissao' ao objeto 'pessoa'
+// e atribua o valor "Engenheira".
