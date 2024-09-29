daf05039067e8b28d8917404b1451940b285f4ef
juliocesardemoraes <jcmcf2012@gmail.com>
2024-09-29 09:22:20 -0300

    feat: add js-stringmanipulation

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// console.log("olá 1");
-// console.log("olá 2");
-// console.log("olá 3");
-// console.log("olá 4");
-// console.log("olá 5");
+// Acessar caracteres de uma string. O primeiro e o último
+let string = "JavascriptB011";
+console.log(string[0]);
+console.log(string[string.length - 1]);
 
-// for (let i = 0; i < 5; i++) {
-//   console.log("olá " + (i + 1));
-// }
+// Contar o número de ocorrências de um caractere, exemplo "a"
+let counterA = 0;
 
-const cores = ["azul", "vermelho", "roxo", "branco", "preto"];
-const pular = "roxo";
-
-for (let i = 0; i < cores.length; i++) {
-  if (pular === cores[i]) {
-    continue;
-  }
-
-  console.log(cores[i]);
+for (let i = 0; i < string.length; i++) {
+  if (string[i] === "a") counterA++;
 }
 
-// cores.map((item) => {
-//   console.log(item);
-// });
+console.log(counterA);
+
+// Contar o número de ocorrências de um caractere, exemplo "B"
+let counterB = 0;
+
+for (let i = 0; i < string.length; i++) {
+  if (string[i] === "B") counterB++;
+}
+
+console.log(counterB);
+
+// Ver se existem números no texto
+
+for (let i = 0; i < string.length; i++) {
+  console.log(isNaN(Number(string[i])) ? "Não é um número" : "É um número");
+}
