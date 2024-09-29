c9563c3773c97d8f803d6fdc63b65e409349ecc5
juliocesardemoraes <jcmcf2012@gmail.com>
2024-09-29 08:13:18 -0300

    feat: add for loop exercise

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// 0 - Crie um array de cores com 4 cores,
-// e um array de frutas com os valores ["Banana", "Pera"]
-const cores = ["azul", "vermelho", "roxo", "branco"];
-const frutas = ["Banana", "Pera"];
+// console.log("olá 1");
+// console.log("olá 2");
+// console.log("olá 3");
+// console.log("olá 4");
+// console.log("olá 5");
 
-// 1. Acesse o segundo e o último elemento do array 'cores'.
+// for (let i = 0; i < 5; i++) {
+//   console.log("olá " + (i + 1));
+// }
 
-// 2. Adicione "Uva" no final do array 'frutas'.
+const cores = ["azul", "vermelho", "roxo", "branco", "preto"];
+const pular = "roxo";
 
-// 3. Adicione "Manga" no início do array 'frutas'.
+for (let i = 0; i < cores.length; i++) {
+  if (pular === cores[i]) {
+    continue;
+  }
 
-// 4. Adicione "Morango" na posição 2 do array 'frutas'.
+  console.log(cores[i]);
+}
 
-// 5. Atualize o terceiro elemento do array 'frutas' para "Laranja".
-
-// 6. Remova o último elemento do array 'frutas'.
-
-// 7. Remova o primeiro elemento do array 'frutas'.
-
-// 8. Remova o segundo elemento do array 'frutas'. (Desafio) Pode procurar no google
-
-// 9. Use um loop 'for' tradicional para imprimir todos os elementos do array 'frutas'.
-
-// 10. Use 'map' para criar um novo array com todas as frutas em maiúsculas.
+// cores.map((item) => {
+//   console.log(item);
+// });
