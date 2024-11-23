280c7d911b12350b6c43367d18c25a56be58091e
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-23 07:53:04 -0300

    feat: add slider animation

--- src/main.js
+++ src/main.js
@@ -1,21 +1,12 @@
-import { products } from "./mockData";
-// 2 Passo puxar  a tabela no js - DOM
-const tableContentTag = document.getElementById("tableContent");
-console.log(tableContentTag.innerHTML);
+const mostrarInfo = () => {
+  const items = document.getElementById("items");
+  items.style.marginLeft = "0px";
+};
 
-for (let i = 0; i < products.length; i++) {
-  tableContentTag.innerHTML += `
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              ${products[i].product}
-            </th>
-            <td class="px-6 py-4">
-            ${products[i].color}</td>
-            <td class="px-6 py-4">${products[i].product}</td>
-            <td class="px-6 py-4">$${products[i].price}</td>
-          </tr>
-`;
-}
+const esconderInfo = () => {
+  const items = document.getElementById("items");
+  items.style.marginLeft = "2000px";
+};
+
+window.mostrarInfo = mostrarInfo;
+window.esconderInfo = esconderInfo;
