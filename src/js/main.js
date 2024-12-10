6039298dd41bc7b7ac923e5a11c0fa92e0cbccc2
juliocesardemoraes <jcmcf2012@gmail.com>
2024-12-10 10:19:44 -0300

    feat: add project

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,12 @@
-// setTimeout(function, delay, ...args)
-const progressBar = document.getElementsByClassName("circular-progress")[0];
-const valueContainer = document.getElementsByClassName("value-container")[0];
+const types = document.getElementsByClassName("type");
 
-let counter = 0;
+const text =
+  "Olá meu nome é júlio, e vou te ensinar a construir um efeito de teclado muito legal!😎";
 
-const idInterval = setInterval(() => {
-  counter++;
-  if (counter <= 100) {
-    valueContainer.textContent = `${counter}%`;
-  } else {
-    clearInterval(idInterval);
-  }
+console.log(text[1]);
 
-  progressBar.style.background = `conic-gradient(#D01D2C ${
-    counter * 3.6
-  }deg, #F2EEE3 ${counter * 7.2}deg)`;
-}, 1000);
-
-// #D01D2C
-//  #F2EEE3
+for (let i = 0; i < text.length; i++) {
+  setTimeout(() => {
+    types[0].textContent += text[i];
+  }, 50 * i);
+}
