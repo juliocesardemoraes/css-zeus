32005d45a30b319928fbf97b947e612a037bd9dc
juliocesardemoraes <jcmcf2012@gmail.com>
2024-12-10 08:58:57 -0300

    feat: add timeout class

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,17 @@
-import { tasks } from "./mock/data";
-import getFormattedDate from "./utils";
+// setTimeout(function, delay, ...args)
+let counter = 0;
 
-const pomolist = document.getElementById("pomolist");
+const incrementCounter = (message) => {
+  counter++;
+  console.log(message + counter);
 
-for (let i = 0; i < tasks.length; i++) {
-  pomolist.innerHTML += `
-       <li class="py-3 sm:py-4">
-              <div class="flex items-center">
-              ${getFormattedDate()}
-                    <img class="icon" src="${
-                      tasks[i].completed === true ? "/check.svg" : "/xmark.svg"
-                    }" />
+  if (counter > 3) {
+    clearTimeout(idCounterTimeout);
+  }
+};
 
-                <div class="flex-1 min-w-0 ms-4">
-                  <p
-                    class="text-sm font-medium text-gray-900 truncate dark:text-white"
-                  >
-                    ${tasks[i].title}
-                  </p>
-                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                    ${tasks[i].description}
+const idCounterTimeout = setTimeout(incrementCounter, 1000);
 
-                  </p>
-                </div>
-                <div
-                  class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
-                >
-                  Pomo: ${tasks[i].pomodoros}
-                </div>
-              </div>
-            </li>
-  `;
-}
+clearTimeout(idCounterTimeout);
+
+const idCounterInterval = setInterval(incrementCounter, 1000, "Timeout 1: ");
--- src/js/utils.js
+++
@@ -1,7 +0,0 @@
-export default function getFormattedDate() {
-  const now = new Date();
-  const day = String(now.getDate()).padStart(2, "0");
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const year = now.getFullYear();
-  return `${day}/${month}/${year}`;
-}
